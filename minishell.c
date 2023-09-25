63ced305c2b80a4e9187249ea441169117a4eac5
Marta Diez Asensio <mdiez-as@car9s4.42barcelona.com>
2023-09-25 18:24:18 +0200

    lines added

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: mdiez-as <mdiez-as@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 14:41:40 by mdiez-as          #+#    #+#             */
-/*   Updated: 2023/09/25 14:46:58 by mdiez-as         ###   ########.fr       */
+/*   Updated: 2023/09/25 16:49:01 by mdiez-as         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,19 @@ int	main(int argc, char **argv)
 
 	while (1) {
 		printf("%s", prompt);
+
+		if ((fgets(cmdline, MAXLINE, stdin) == NULL) && ferror(stdin))
+			error("fgets error");
+		if (feof(stdin))
+		{
+			printf("\n");
+			exit(0);
+		}
+
+		// Remove trailing newline
+		cmdline[strlen(cmdline) - 1] = '\0';
+
+		// Evaluate command line
+		eval(cmdline);
 	}
 }
